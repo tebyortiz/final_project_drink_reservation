2a85c17fa69b633e62e951a2d6f168b9f8837c20
tebyortiz <tebyortiz@gmail.com>
2023-11-02 23:51:56 -0300

    Map Added

--- src/components/clients-components/ClientWelcome.tsx
+++ src/components/clients-components/ClientWelcome.tsx
@@ -1,8 +1,70 @@
-import { Card, Divider, Grid, Typography } from "@mui/material";
+import { useState } from "react";
+import {
+  Accordion,
+  AccordionDetails,
+  AccordionSummary,
+  Button,
+  Card,
+  Divider,
+  Grid,
+  Typography,
+} from "@mui/material";
 import { Box } from "@mui/system";
 import clienteBanner from "../../../public/images/clientebanner.png";
+import GoogleMapReact from "google-map-react";
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "../../models/RootStateTypes";
+import { updatePosition } from "../../redux/ClientsSlice";
+import { MarkerPosition } from "../../models/UsersModels";
+import RoomTwoToneIcon from "@mui/icons-material/RoomTwoTone";
+
+const defaultCenter = {
+  lat: -32.8897,
+  lng: -68.844629,
+};
+
+const Marker = ({ position }: { position: MarkerPosition }) => (
+  <div className="marker" style={{ position: "relative", left: -15, top: -15 }}>
+    <RoomTwoToneIcon
+      style={{
+        width: 30,
+        height: 30,
+        color: "red",
+        transform: "translate(-50%, -100%)",
+      }}
+    />
+  </div>
+);
 
 const ClientWelcome = () => {
+  const dispatch = useDispatch();
+  const [isMapOpen, setMapOpen] = useState(false);
+
+  const user = useSelector((state: RootState) => state.user.user);
+  const clients = useSelector((state: RootState) => state.clients.clients);
+  const userName = user?.name;
+  const selectedClient = clients.find((client) => client.name === userName);
+
+  const [newPosition, setNewPosition] = useState<MarkerPosition | null>(
+    selectedClient?.markerPosition || null
+  );
+
+  const handleMarkerPositionChange = (position: MarkerPosition) => {
+    setNewPosition(position);
+  };
+
+  const handleSaveLocation = () => {
+    if (userName && newPosition) {
+      dispatch(updatePosition({ clientName: userName, newPosition }));
+      setNewPosition({ ...newPosition });
+    }
+  };
+
+  const handleMapClick = () => {
+    setMapOpen(!isMapOpen);
+  };
+  
+
   return (
     <Box
       display="flex"
@@ -13,6 +75,41 @@ const ClientWelcome = () => {
       justifyContent="space-between"
       marginTop="50px"
     >
+      <Grid item xs={12}>
+        <Accordion expanded={isMapOpen} onChange={handleMapClick}>
+          <AccordionSummary>
+            <Typography variant="h4">Mi Ubicación</Typography>
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={handleSaveLocation}
+            >
+              Guardar Ubicación
+            </Button>
+          </AccordionSummary>
+          <AccordionDetails>
+            <div style={{ height: "400px", width: "100%" }}>
+              <GoogleMapReact
+                bootstrapURLKeys={{
+                  key: "AIzaSyBfjO7sxd8P6HDrF1lmvLV151z7ocauPD0",
+                }}
+                defaultCenter={defaultCenter}
+                defaultZoom={11}
+                onClick={(event) => {
+                  const newPosition: MarkerPosition = {
+                    lat: (event.lat),
+                    lng: (event.lng),
+                  };
+                  handleMarkerPositionChange(newPosition);
+                }}
+              >
+                {newPosition && <Marker position={newPosition} />}
+              </GoogleMapReact>
+            </div>
+          </AccordionDetails>
+        </Accordion>
+      </Grid>
+
       <Grid container item xs={12}>
         <Card
           sx={{
